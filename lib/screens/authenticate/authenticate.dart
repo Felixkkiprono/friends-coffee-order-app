1f6be754cacc85028ab743ffc5b5d048796cfe0b
Felixkkiprono <kandagorfelix@gmail.com>
2020-02-08 15:52:42 +0300

    third commit

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,16 +1,25 @@
 import 'package:brew_crew/screens/authenticate/register.dart';
+import 'package:brew_crew/screens/authenticate/signin.dart';
 import 'package:flutter/material.dart' ;
 
 class Authenticate extends StatefulWidget {
+
   @override
   _AuthenticateState createState() => _AuthenticateState();
 }
 
 class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+void toggleView(){
+  setState(()=> showSignIn = !showSignIn);
+}
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Register(),
-    );
+    if (showSignIn) {
+      return SignIn(toggleView: toggleView);
+    } else {
+      return Register(toggleView: toggleView);
+    }
   }
 }
