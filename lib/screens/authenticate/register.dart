32336d8c7270d5fb452c2cda77c76dd71da96be1
Felixkkiprono <kandagorfelix@gmail.com>
2020-02-08 15:24:19 +0300

    second_commit

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,27 +1,26 @@
-//import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+//import 'package:brew_crew/services/auth.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
+class _RegisterState extends State<Register> {
 
   //final AuthService _auth = AuthService();
+
   //text field state
   String email ='';
   String password='';
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       backgroundColor: Colors.brown[200],
       appBar: AppBar(
         backgroundColor: Colors.brown[600],
         elevation: 0.0,
-        title: Text('Sign In To Brew Crew'),
+        title: Text('Sign Up To Brew Crew'),
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0,horizontal:50.0),
@@ -47,7 +46,7 @@ class _SignInState extends State<SignIn> {
                   print(password);
                 },
                 color: Colors.grey[300],
-                child: Text('Sign In'),)
+                child: Text('Register'),)
             ],
           ),
         )
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,15 +1,27 @@
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
+class Home extends StatelessWidget {
 
-class _HomeState extends State<Home> {
+  final AuthService _auth =AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home'),
+    return Scaffold(
+      backgroundColor: Colors.brown[600],
+      appBar: AppBar(
+        title: Text('Brew Crew'),
+        backgroundColor: Colors.brown[700],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed:() async{
+              await _auth.signOut();
+            },
+             icon:  Icon(Icons.person),
+              label: Text('Log Out'))
+        ],
+      ),
     );
   }
 }
